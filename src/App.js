5e8c4eaa986a31776e37e42c7147c27e39caa0a4
Pham Linh <phamvuonglinh2000@gmail.com>
2022-12-22 21:30:46 +0700

    new file:   .babelrc 	new file:   config-overrides.js 	new file:   jsconfig.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/index.js 	new file:   src/pages/Contact.js 	new file:   src/pages/Home.js 	new file:   src/pages/News.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import HomePages from './pages/Home'
+import NewsPages from './pages/News'
+import ContactPages from './pages/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+        <Route path="/news" element={<NewsPages />} />
+        <Route path="/contact" element={<ContactPages />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
