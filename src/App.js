d6566f8b9c1fc82287318d2ddced4aa361e8593e
Pham Linh <phamvuonglinh2000@gmail.com>
2022-12-23 10:43:44 +0700

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.js 	new file:   src/components/GlobalStyles/GlobalStyles.scss 	new file:   src/components/GlobalStyles/index.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import HomePages from './pages/Home';
-import NewsPages from './pages/News';
-import ContactPages from './pages/Contact';
-
 function App() {
     return (
         <div className="app">
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/news">News</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/" element={<HomePages />} />
-                <Route path="/news" element={<NewsPages />} />
-                <Route path="/contact" element={<ContactPages />} />
-            </Routes>
+            <h1>React App</h1>
         </div>
     );
 }
