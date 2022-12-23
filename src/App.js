6c2d1b4c44fd3532db96a358e465dfe2a08c480c
Pham Linh <phamvuonglinh2000@gmail.com>
2022-12-23 09:51:07 +0700

    new file:   .prettierrc 	new file:   .vscode/settings.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import HomePages from './pages/Home'
-import NewsPages from './pages/News'
-import ContactPages from './pages/Contact'
+import { Routes, Route, Link } from 'react-router-dom';
+import HomePages from './pages/Home';
+import NewsPages from './pages/News';
+import ContactPages from './pages/Contact';
 
 function App() {
-  return (
-    <div className="app">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/news">News</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<HomePages />} />
-        <Route path="/news" element={<NewsPages />} />
-        <Route path="/contact" element={<ContactPages />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div className="app">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<HomePages />} />
+                <Route path="/news" element={<NewsPages />} />
+                <Route path="/contact" element={<ContactPages />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
