04a94fab5cf2cac611f8dc3e6b18bc5041de129f
Pham Linh <phamvuonglinh2000@gmail.com>
2022-12-27 09:24:45 +0700

    modified:   package-lock.json 	modified:   package.json 	new file:   src/components/AccountItem/AccountItem.module.scss 	new file:   src/components/AccountItem/index.js 	new file:   src/components/Button/Button.module.scss 	new file:   src/components/Button/index.js 	modified:   src/components/GlobalStyles/GlobalStyles.scss 	modified:   src/components/Layout/components/Header/Header.module.scss 	modified:   src/components/Layout/components/Header/index.js 	new file:   src/components/Popper/Menu/Menu.module.scss 	new file:   src/components/Popper/Menu/MenuItem.js 	new file:   src/components/Popper/Menu/index.js 	new file:   src/components/Popper/Popper.module.scss 	new file:   src/components/Popper/Wrapper.js 	new file:   src/components/Popper/index.js

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/6709c6da43ef4429b97dff76ef893b7d~c5_100x100.jpeg?x-expires=1672149600&x-signature=p0x0i%2FI%2Fmcu4IQWRwbSPpttYP6U%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>NguyenVanA</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
